26a834bde254703827e0ccff200d1913a84a9df9
mpakus <mrak69@gmail.com>
2018-02-11 18:28:28 +0700

    carcass

---
+++ spec/crypto_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Crypto do
+  it "has a version number" do
+    expect(Crypto::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
